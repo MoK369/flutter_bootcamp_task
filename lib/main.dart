aa672c8782058d20912e09b26c514ed922c941a3
mohammedkhalil <klilmohammed9@gmail.com>
2025-01-27 20:16:36 +0200

    Commit Date: Jan 27, 2025 change: - ensuring ScreenUtil package got initialized and ensuring screen size is known before the application starts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bootcamp_task/di.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_bootcamp_task/presentation/core/themes/app_themes.dart';
 import 'package:flutter_bootcamp_task/presentation/products/screen/products_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  FlutterNativeSplash.preserve(
+      widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
   configureDependencies();
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(
-      designSize: const Size(430,932),
+    ScreenUtil.init(context);
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
       minTextAdapt: true,
       splitScreenMode: true,
       child: MaterialApp(
